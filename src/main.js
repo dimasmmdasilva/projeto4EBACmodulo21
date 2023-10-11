8c6110754a18743e4cc7f917ee7381e5023fc34d
Dimas Marcondes Moreira da Silva <dimasmarcondes88@gmail.com>
2023-10-11 12:54:50 -0300

    upload clone Disney+ EBAC modulo 21

--- src/main.js
+++ src/main.js
@@ -3,7 +3,26 @@ document.addEventListener
     function()
     {
         const buttons = document.querySelectorAll('[data-tab-button]');
+        const questions = document.querySelectorAll('[data-faq-question]');
         
+        const heroSection = document.querySelector('.hero');
+        const alturaHero = heroSection.clientHeight;
+
+        window.addEventListener('scroll', function()
+        {
+            const posicaoAtual = window.scrollY;
+
+            if (posicaoAtual < alturaHero)
+            {
+                ocultaElementoHeader();
+            }
+            else
+            {
+                exibeElementoHeader();
+            }
+        })
+
+        // seção de atrações, programação das abas
         for (let i = 0; i < buttons.length; i++)
         {
             buttons[i].addEventListener
@@ -18,8 +37,35 @@ document.addEventListener
                 }
             )
         }
+
+        // seção FAQ, accordion
+        for (let i = 0; i < questions.length; i++)
+        {
+            questions[i].addEventListener
+            ('click', abreOUfecha);
+        }
     }
 )
+
+    function ocultaElementoHeader ()
+    {
+        const header = document.querySelector('header');
+        header.classList.add('header__hidden');
+    }
+
+    function exibeElementoHeader ()
+    {
+        const header = document.querySelector('header');
+        header.classList.remove('header__hidden');
+    }
+
+    function abreOUfecha(elemento)
+    {
+        const classe = 'faq__questions__item--is-open';
+        const elementoPai = elemento.target.parentNode;
+
+        elementoPai.classList.toggle(classe);
+    }
 
     function removeBotaoAtivo()
         {
